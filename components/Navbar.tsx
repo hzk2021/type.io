bab248187d0ea1e71019e526c0e6b1451a40b287
hzk2021 <hezhenkai1999@gmail.com>
2023-07-22 22:45:37 +0800

    Add options functiality

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,8 +4,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faKeyboard, faCrown, faUser, faT } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import Link from 'next/link';
+import { useContext } from 'react';
+import { PreferenceContext, preferenceDefinition } from '@context/PreferenceContext';
 
 function Navbar() {
+  const preference = useContext(PreferenceContext);
+
+  function handleChange(values : preferenceDefinition) {
+    preference?.setPreference({
+      wordType: values.wordType,
+      time: values.time
+    });
+  }
+
   return (
     <nav className='flex w-full mt-2 mb-16 pt-3 gap-5 items-center justify-center flex-wrap sm:justify-between'>
         <div className='flex gap-4 text-lg'>
@@ -27,15 +38,15 @@ function Navbar() {
 
         <div className='flex options flex-col text-xs cursor-pointer'>
             <div className='flex option-type gap-2'>
-              <span className='hover:text-white'>words</span>
-              <span className='hover:text-white'>sentences</span>
-              <span className='hover:text-white'>numbers</span>
+              <span className={`hover:text-white ${preference?.wordType === 'words' ? 'text-white' : ''}`} onClick={() => handleChange({time: preference!.time, wordType: "words"})}>words</span>
+              <span className={`hover:text-white ${preference?.wordType === 'sentences' ? 'text-white' : ''}`} onClick={() => handleChange({time: preference!.time, wordType: "sentences"})}>sentences</span>
+              <span className={`hover:text-white ${preference?.wordType === 'numbers' ? 'text-white' : ''}`} onClick={() => handleChange({time: preference!.time, wordType: "numbers"})}>numbers</span>
             </div>
 
             <div className='flex option self-end gap-2'>
-              <span className='hover:text-white'>15</span>
-              <span className='hover:text-white'>30</span>
-              <span className='hover:text-white'>60</span>
+              <span className={`hover:text-white ${preference?.time === 15 ? 'text-white' : ''}`} onClick={() => handleChange({time: 15, wordType: preference!.wordType})}>15</span>
+              <span className={`hover:text-white ${preference?.time === 30 ? 'text-white' : ''}`} onClick={() => handleChange({time: 30, wordType: preference!.wordType})}>30</span>
+              <span className={`hover:text-white ${preference?.time === 60 ? 'text-white' : ''}`} onClick={() => handleChange({time: 60, wordType: preference!.wordType})}>60</span>
             </div>
         </div>
     </nav>

f8d35210a7c1cdd93227f3428bf5b10685091629
hzk2021 <hezhenkai1999@gmail.com>
2023-07-30 20:58:23 +0800

    Change route to dynamic rendering

--- hooks/useFetch.tsx
+++ hooks/useFetch.tsx
@@ -8,7 +8,7 @@ function useFetch<T = any | undefined>(method : string, url: string, body ?: str
     const fetchData = async () => {
       const data = await (await fetch(url, {
       method,
-      cache: 'no-store', 
+      next: {revalidate: 20},
       body
       })).json();
       
